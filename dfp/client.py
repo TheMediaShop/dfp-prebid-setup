f27fab1bbf2878142e43ad51ef1084749effdffb
Austin Bischoff <austin.bischoff@gmail.com>
2018-11-12 14:41:27 -0600

    Dump yaml googleads.yaml in favor of using settings.py to define these values.  Annoying having to edit two files when updating multiple dfp accounts.

--- dfp/client.py
+++ dfp/client.py
@@ -1,8 +1,17 @@
-
 from googleads import dfp
+import yaml
 
 import settings
 
 
 def get_client():
-  return dfp.DfpClient.LoadFromStorage(settings.GOOGLEADS_YAML_FILE)
+    # Build the Yaml file from scratch so we can move the settings into the application level
+    yaml_data = {
+        'dfp': {
+            'application_name': settings.DFP_APPLICATION_NAME,
+            'network_code': settings.DFP_NETWORK_CODE,
+            'path_to_private_key_file': settings.DFP_CREDENTIALS_JSON
+        }
+    }
+
+    return dfp.DfpClient.LoadFromString(yaml.dump(yaml_data))
